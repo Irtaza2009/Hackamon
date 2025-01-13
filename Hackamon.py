7b4a570b503df21870a669555e81352d7c552eb9
Irtaza <irtazanaqvi05@gmail.com>
2025-01-13 17:22:14 +0500

    Added full charging functionality and charging animation with a charging station and charging state and scene

--- Hackamon.py
+++ Hackamon.py
@@ -42,6 +42,17 @@ hackamon_sprite_jump = displayio.TileGrid(hackamon_sheet_jump,
                                         x=(display.width - tile_width) // 2,
                                         y=display.height - tile_height - 40)
 
+hackamon_sheet_charging = displayio.OnDiskBitmap("Hackamon-1-Charging-Spritesheet.bmp")
+hackamon_sprite_charging = displayio.TileGrid(hackamon_sheet_charging,
+                                        pixel_shader=hackamon_sheet_charging.pixel_shader,
+                                        width=1,
+                                        height=1,
+                                        tile_width=tile_width,
+                                        tile_height=tile_height,
+                                        default_tile=0,
+                                        x=85,
+                                        y=70)
+
 button_sheet = displayio.OnDiskBitmap("Button-1-Spritesheet.bmp")
 
 button_sprite = displayio.TileGrid(button_sheet,
@@ -86,6 +97,17 @@ splash.append(button_sprite)
 
 splash.append(hackamon_sprite_idle)
 
+charging_station_sheet = displayio.OnDiskBitmap("Charging-Station.bmp")
+charging_station_sprite = displayio.TileGrid(charging_station_sheet,
+                                    pixel_shader=charging_station_sheet.pixel_shader,
+                                    width=1,
+                                    height=1,
+                                    tile_width=55,
+                                    tile_height=42,
+                                    default_tile=0,
+                                    x=72,
+                                    y=70)
+
 frame = 0
 speed = 4
 game_over = False
@@ -94,6 +116,8 @@ facing_left = True
 gameState = "Main"
 happiness = 5000
 battery = 5000
+charging = False
+chargingSprite = False
 
 
 def run_jump_animation():
@@ -137,11 +161,31 @@ def check_button_press():
         button_sprite[0] = 1  # Switch button to pressed state
         print("Button Pressed!")
         time.sleep(0.5)
-        gameState = "MiniGame"
-        mini_game()
+        gameState = "Station"
+        to_mini_game()
+
+def charging_station():
+    global battery, facing_left, charging, chargingSprite
+    if check_collision(
+        hackamon_sprite_idle, charging_station_sprite,
+        tile_width, tile_height, 55, 42
+    ):
+        print("Charging!")
+        if facing_left == False:
+            facing_left = True
+            hackamon_sprite_idle.flip_x = False
+            hackamon_sprite_jump.flip_x = False
+
+        splash.append(hackamon_sprite_charging)
+
+
+        hackamon_sprite_idle.x = 85
+        hackamon_sprite_idle.y = 70
+        charging = True
+        chargingSprite = True
 
 def manage_stats():
-    global happiness, battery, game_over
+    global happiness, battery, game_over, charging
     if happiness <= 0 or battery <= 0:
         print("Game Over!")
         game_over = True
@@ -149,11 +193,17 @@ def manage_stats():
         happiness -= 1
         print("Happiness: " + str(happiness))
         happiness_bar_sprite[0] = 4 - math.ceil(happiness // 1000)
-        battery -= 1
+        if charging:
+            if battery < 4999:
+                battery += 1
+            elif battery == 4999:
+                charging = False
+        else:
+            battery -= 1
         print("Battery: " + str(battery))
         battery_bar_sprite[0] = 4 - math.ceil(battery // 1000)
         
-def mini_game():
+def to_mini_game():
     global gameState
     # remove all from splash
     splash.remove(hackamon_sprite_idle)
@@ -162,9 +212,12 @@ def mini_game():
     splash.remove(battery_bar_sprite)
     # add station background and sprites back
     splash.append(station_bg_sprite)
+    splash.append(charging_station_sprite)
     splash.append(hackamon_sprite_idle)
     splash.append(happiness_bar_sprite)
     splash.append(battery_bar_sprite)
+
+
     
     
 
@@ -184,7 +237,8 @@ while True:
 
     if game_over == False:
         if keys[pygame.K_LEFT]:
-            if (gameState == "Main" and hackamon_sprite_idle.x > 24) or (gameState == "MiniGame" and hackamon_sprite_idle.x > 0):
+            charging = False
+            if (gameState == "Main" and hackamon_sprite_idle.x > 24) or (gameState == "Station" and hackamon_sprite_idle.x > 0):
                 facing_left = True
                 hackamon_sprite_idle.x -= speed
                 hackamon_sprite_jump.x -= speed
@@ -192,7 +246,8 @@ while True:
                 hackamon_sprite_jump.flip_x = False
 
         if keys[pygame.K_RIGHT]:
-            if (gameState == "Main" and hackamon_sprite_idle.x < 78) or (gameState == "MiniGame" and hackamon_sprite_idle.x < 128 - tile_width):
+            charging = False
+            if (gameState == "Main" and hackamon_sprite_idle.x < 78) or (gameState == "Station" and hackamon_sprite_idle.x < 128 - tile_width):
                 facing_left = False
                 hackamon_sprite_idle.x += speed
                 hackamon_sprite_jump.x += speed
@@ -201,21 +256,31 @@ while True:
 
         # For testing! I know there will be only 3 buttons :)
         if keys[pygame.K_UP]:
-            if (gameState == "Main" and hackamon_sprite_idle.y > 64 - 20) or (gameState == "MiniGame" and hackamon_sprite_idle.y > 96 - 20):
+            charging = False
+            if (gameState == "Main" and hackamon_sprite_idle.y > 64 - 20) or (gameState == "Station" and hackamon_sprite_idle.y > 96 - 20):
                 hackamon_sprite_idle.y -= speed
                 hackamon_sprite_jump.y -= speed
                 
         if keys[pygame.K_DOWN]:
-            if (gameState == "Main" and hackamon_sprite_idle.y < 92 - tile_height) or (gameState == "MiniGame" and hackamon_sprite_idle.y < 128 - tile_height):
+            charging = False
+            if (gameState == "Main" and hackamon_sprite_idle.y < 92 - tile_height) or (gameState == "Station" and hackamon_sprite_idle.y < 128 - tile_height):
                 hackamon_sprite_idle.y += speed
                 hackamon_sprite_jump.y += speed
         
         if keys[pygame.K_SPACE] and not isJumping:
             isJumping = True
+            charging = False
             splash.remove(hackamon_sprite_idle)
             splash.append(hackamon_sprite_jump)
             run_jump_animation()
             check_button_press()
+            charging_station()
+
+        
+        
+        if chargingSprite == True and not charging:
+            chargingSprite = False
+            splash.remove(hackamon_sprite_charging)
 
         manage_stats()
 
@@ -223,6 +288,8 @@ while True:
 
 
 
+    if charging:
+        hackamon_sprite_charging[0] = frame 
 
     hackamon_sprite_idle[0] = frame
     frame = (frame + 1) % (hackamon_sheet_idle.width // tile_width)
